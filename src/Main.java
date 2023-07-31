fb4df274a38fe6a6c51f31ffd366cf606d89aab3
wanghdong <2542447605@qq.com>
2023-07-31 11:13:28 +0800

    版本1.2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("版本1.0");
         System.out.println("版本1.1");
+        System.out.println("版本1.2");
     }
 }
